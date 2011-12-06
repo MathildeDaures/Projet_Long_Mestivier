77b9cb567337b476af3fa09a4cb43d04896bce32
Mathilde Daures <mathilde.daures@gmail.com>
2011-12-06 23:05:31 +0100

    gestion modif POR - opacity

--- gmltovpyt.py
+++ gmltovpyt.py
@@ -7,6 +7,8 @@ from visual import *
 import csv
 import time
 
+#autocenter()
+
 # Récuperation des données de parsing du fichier gml
 filin = open("parsedata.dat")
 res = pickle.load(filin)
@@ -96,33 +98,112 @@ for row in POE:
 
 #print POE_value
     
-# Modification par temps de simulation
+# Modification par temps de simulation --  POE
 
+#nb_simul = len(POE_value.keys())
+#print nb_simul
+
+#incr = 1
+#tps = time.clock()
+#while incr <= nb_simul-1:
+#    print incr
+#    print tps
+#    for specie in POE_value[incr]:
+#        value = POE_value[incr][specie]
+        #print value  
+#        if specie != 'nOfEvents' and specie != 'time' and specie != 'NONE':
+#            id_tochange = labels[specie]
+#            if float(value) < 10:
+#                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=(0, 100, 0))      #Dark green
+#            elif float(value) >= 10 and float(value) < 20:
+#                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=(34, 139, 34))    #Forest green              
+#            elif float(value) >= 20 and float(value) < 30:
+#                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=(50, 205, 50))    #Lime green
+#            elif float(value) >= 30 and float(value) < 40:
+#                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=(173, 255, 47))   #Green Yellow
+#            elif float(value) >= 40 and float(value) < 50:
+#                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=(255, 255, 0))    #Yellow            
+#            elif float(value) >= 50 and float(value) < 60:
+#                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=(255, 165, 47))   #Orange
+#            elif float(value) >= 60 and float(value) < 70:
+#                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=(255, 69, 0))     #Orange Red
+#            elif float(value) >= 70 and float(value) < 80:
+#                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=(255, 0, 0))      #Red
+#            elif float(value) >= 80 and float(value) < 90:
+#                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=(205, 0, 0))      #Red 3
+#            elif float(value) >= 90:  
+#                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=(139, 0, 0))      #Red 4
+        
+#        newtime = time.clock()        
+#        if newtime >= tps + 2:
+#            print "in time!"
+#            incr = incr + 1
+#            tps = newtime      
+
+# Récupération des valeurs de simulation pour les rules
+POR = csv.reader(open("./Visu/Simulations/res_mutation_nwin_100_winsize_500000_nsim_500_Ri_96.por","rb"), delimiter='\t', quotechar='.')
+rules_list = csv.reader(open("./Visu/Modele/model_14_03_2010_listOfRules_modified.csv", "rb"))
+
+rule_list = []
+for ru in rules_list:
+    rule_list.append(ru)
+
+#print rule_list
+
+POR_value = {}
+cpt = 1
+for row in POR:
+    POR_ok = {}
+    column = 0
+    for ru in rule_list:     
+        POR_ok[ru[0]] = row[column]
+        column = column + 1
+    POR_value[cpt] = POR_ok
+    cpt = cpt + 1
+
+#print POR_value
+
+# Modification par temps de simulation --  POR
 nb_simul = len(POE_value.keys())
 print nb_simul
 
 incr = 1
 tps = time.clock()
-while incr <= nb_simul:
+while incr <= nb_simul-1:
     print incr
     print tps
-    for specie in POE_value[incr]:
-        value = POE_value[incr][specie]
+    for rule in POR_value[incr]:
+        value = POR_value[incr][rule]
         #print value  
-        if specie != 'nOfEvents' and specie != 'time' and specie != 'NONE':
-            id_tochange = labels[specie]
-            if float(value) <= 20:
-                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=(255, 255, 0))
-            elif float(value) > 80:  
-                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=(255, 0, 0))
-            else:
-                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=color.green)
-        newtime = time.clock()        
+        if rule != 'nOfEvents' and rule != 'time' and rule != 'NONE':
+            id_tochange = labels[rule]
+            if float(value) < 0.0001:
+                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=color.blue, opacity=0.1)     
+            elif float(value) >= 0.0001 and float(value) < 0.0005:
+                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=color.blue, opacity=0.2)               
+            elif float(value) >= 0.0005 and float(value) < 0.001:
+                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=color.blue, opacity=0.3)  
+            elif float(value) >= 0.001 and float(value) < 0.005:
+                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=color.blue, opacity=0.4)  
+            elif float(value) >= 0.005 and float(value) < 0.01:
+                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=color.blue, opacity=0.5)             
+            elif float(value) >= 0.01 and float(value) < 0.05:
+                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=color.blue, opacity=0.6)  
+            elif float(value) >= 0.05 and float(value) < 0.1:
+                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=color.blue, opacity=0.7)  
+            elif float(value) >= 0.1 and float(value) < 0.5:
+                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=color.blue, opacity=0.8)  
+            elif float(value) >= 0.5 and float(value) < 1:
+                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=color.blue, opacity=0.9)   
+            else:  
+                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=color.blue, opacity=1)   
+        
+        newtime = time.clock()
+        print newtime        
         if newtime >= tps + 2:
             print "in time!"
             incr = incr + 1
             tps = newtime      
-
 
 
 
