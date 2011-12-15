694ddd4cde68039d355e7c818568e3d6ce655a67
Mathilde Daures <mathilde.daures@gmail.com>
2011-12-15 13:59:32 +0100

    to be sure

--- gmltovpyt.py
+++ gmltovpyt.py
@@ -9,6 +9,8 @@ import time
 
 #autocenter()
 
+scene = display(title='Simulation', center=(1871.7626953125,1621.0,0), userspin = False)
+#scene.fullscreen = True
 # Récuperation des données de parsing du fichier gml
 filin = open("parsedata.dat")
 res = pickle.load(filin)
@@ -125,66 +127,108 @@ nb_simul = len(POE_value.keys())
 
 incr = 1
 tps = time.clock()
-while incr <= nb_simul-1:
+while incr <= nb_simul:#-1:
     print incr
     print tps
+    nosim = str(incr)
+    box(pos=(200,3000,0), color=color.red)
+    label(pos=(200,3000,0), text=nosim)
+
+    
+    
     for specie in POE_value[incr]:
         value = POE_value[incr][specie]
         #print value  
         if specie != 'nOfEvents' and specie != 'time' and specie != 'NONE':
             id_tochange = labels[specie]
             if float(value) < 10:
-                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=(0, 0.392, 0))      #Dark green
+                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius = 15, color=(0, 0.392, 0))      #Dark green
+                label(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), text = specie, box = False, line = False, space = 0, height=10, border = 0)        
             elif float(value) >= 10 and float(value) < 20:
-                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=(0.133, 0.545, 0.133))    #Forest green              
+                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius = 15, color=(0.133, 0.545, 0.133))    #Forest green              
+                #label(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), text = specie, box = False, line = False, space = 0, height=10, border = 0)
             elif float(value) >= 20 and float(value) < 30:
-                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=(0.196, 0.804, 0.196))    #Lime green
+                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius = 15, color=(0.196, 0.804, 0.196))    #Lime green
+                #label(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), text = specie, box = False, line = False, space = 0, height=10, border = 0)
             elif float(value) >= 30 and float(value) < 40:
-                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=(0.678, 1, 0.184))   #Green Yellow
+                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius = 15, color=(0.678, 1, 0.184))   #Green Yellow
+                #label(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), text = specie, box = False, line = False, space = 0, height=10, border = 0)
             elif float(value) >= 40 and float(value) < 50:
-                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=(1, 1, 0))    #Yellow            
+                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius = 15, color=(1, 1, 0))    #Yellow 
+                #label(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), text = specie, box = False, line = False, space = 0, height=10, border = 0)           
             elif float(value) >= 50 and float(value) < 60:
-                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=(1, 0.647, 0))   #Orange
+                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius = 15, color=(1, 0.647, 0))   #Orange
+                #label(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), text = specie, box = False, line = False, space = 0, height=10, border = 0)
             elif float(value) >= 60 and float(value) < 70:
-                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=(1, 0.271, 0))     #Orange Red
+                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius = 15, color=(1, 0.271, 0))     #Orange Red
+                #label(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), text = specie, box = False, line = False, space = 0, height=10, border = 0)
             elif float(value) >= 70 and float(value) < 80:
-                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=(1, 0, 0))      #Red
+                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius = 15, color=(1, 0, 0))      #Red
+                #label(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), text = specie, box = False, line = False, space = 0, height=10, border = 0)
             elif float(value) >= 80 and float(value) < 90:
-                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=(0.804, 0, 0))      #Red 3
+                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius = 15, color=(0.804, 0, 0))      #Red 3
+                #label(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), text = specie, box = False, line = False, space = 0, height=10, border = 0)
             elif float(value) >= 90:  
-                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=(0.545, 0, 0))      #Red4 
+                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius = 15, color=(0.545, 0, 0))      #Red4
+                #label(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), text = specie, box = False, line = False, space = 0, height=10, border = 0)
+   
     for rule in POR_value[incr]:
         value = POR_value[incr][rule]
         #print value  
         if rule != 'nOfEvents' and rule != 'time' and rule != 'NONE':
             id_tochange = labels[rule]
             if float(value) < 0.0001:
-                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=color.blue, opacity=0.1)     
+                rule = box(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), length=20, height=20, width=20, color=(0.686, 0.933, 0.933))     
+                #label(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), text = specie, box = False, line = False, space = 0, height=10, border = 0)
             elif float(value) >= 0.0001 and float(value) < 0.0005:
-                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=color.blue, opacity=0.2)               
+                rule = box(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), length=20, height=20, width=20, color=(0.69, 0.878, 0.902))               
+                #label(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), text = specie, box = False, line = False, space = 0, height=10, border = 0)
             elif float(value) >= 0.0005 and float(value) < 0.001:
-                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=color.blue, opacity=0.3)  
+                rule = box(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), length=20, height=20, width=20, color=(0.678, 0.847, 0.902))  
+                #label(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), text = specie, box = False, line = False, space = 0, height=10, border = 0)
             elif float(value) >= 0.001 and float(value) < 0.005:
-                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=color.blue, opacity=0.4)  
+                rule = box(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), length=20, height=20, width=20, color=(0.529, 0.808, 0.98))  
+                #label(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), text = specie, box = False, line = False, space = 0, height=10, border = 0)
             elif float(value) >= 0.005 and float(value) < 0.01:
-                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=color.blue, opacity=0.5)             
+                rule = box(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), length=20, height=20, width=20, color=(0, 0.749, 0.922))             
+                #label(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), text = specie, box = False, line = False, space = 0, height=10, border = 0)
             elif float(value) >= 0.01 and float(value) < 0.05:
-                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=color.blue, opacity=0.6)  
+                rule = box(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), length=20, height=20, width=20, color=(0.117, 0.0565, 0.922))  
+                #label(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), text = specie, box = False, line = False, space = 0, height=10, border = 0)
             elif float(value) >= 0.05 and float(value) < 0.1:
-                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=color.blue, opacity=0.7)  
+                rule = box(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), length=20, height=20, width=20, color=(0.255, 0.0412, 0.922))  
+                #label(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), text = specie, box = False, line = False, space = 0, height=10, border = 0)
             elif float(value) >= 0.1 and float(value) < 0.5:
-                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=color.blue, opacity=0.8)  
+                rule = box(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), length=20, height=20, width=20, color=(0.416, 0.353, 0.804))  
+                #label(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), text = specie, box = False, line = False, space = 0, height=10, border = 0)
             elif float(value) >= 0.5 and float(value) < 1:
-                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=color.blue, opacity=0.9)   
+                rule = box(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), length=20, height=20, width=20, color=(0.282, 0.239, 0.545))   
+                #label(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), text = specie, box = False, line = False, space = 0, height=10, border = 0)
             else:  
-                rule = sphere(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), radius=15, color=color.blue, opacity=1)   
-               
-        newtime = time.clock()        
-        if newtime >= tps + 2:
-            print "in time!"
-            incr = incr + 1
-            tps = newtime      
-
+                rule = box(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), length=20, height=20, width=20, color=(0, 0, 0.502))   
+                #label(pos=(node_id[id_tochange]['x'], node_id[id_tochange]['y']), text = specie, box = False, line = False, space = 0, height=10, border = 0)
+        
+        newtime = time.clock() 
+     
+        if scene.kb.keys: 
+            s = scene.kb.getkey() 
+            if s == 'n': 
+                incr = incr + 1
+                tps = newtime 
+            elif s == 'p': 
+                incr = incr - 1
+                tps = newtime
+            elif s == '1' or s == '2' or s == '3' or s == '4' or s == '5' or s == '6' or s == '7' or s == '8' or s == '9' or len(s) >= '2':
+                incr = int(s) 
+            elif s == 'toto':
+                print "---------------------------TOTOTOTO------------------------------"            
+            else:
+                print "Les seules entrées valables sont n pour avancer et p pour reculer" 
+                tps = newtime 
+        #elif newtime >= tps + 60:
+        #    print "Aucune entrée enregistrée -- Passage à la fenètre suivante"
+        #    incr = incr + 1
+        #    tps = newtime 
 
 
 
