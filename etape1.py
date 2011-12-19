0fd3301cc8d80a5859fae88d090f2cba04ec6458
Mathilde Daures <mathilde.daures@gmail.com>
2011-12-19 19:46:50 +0100

    intégration fct et use mod

--- etape1.py
+++ etape1.py
@@ -3,8 +3,11 @@
 
 import pickle
 import re
-from visual import box, color, sphere, arrow
+from visual import box, color, sphere, arrow, display
 #from random import randrange
+import mod_create_graph as mcg
+import csv
+import mod_read_simul as mrs
 
 # Récuperation des données de parsing du fichier gml
 FILIN = open("parsedata.dat")
@@ -30,8 +33,15 @@ for element in GML_CONTENT:
 #------------------------------------------------------------------------------#
 
 #------------------------------------------------------------------------------#
-# NODES
-#----------------------
+# Paramètres du graph
+#------------------------
+SCENE = display(title='Simulation', center=(1871.7626953125, 1621.0, 0), \
+                userspin = False, background=(1, 1, 1))
+#------------------------------------------------------------------------------#
+
+#------------------------------------------------------------------------------#
+# Création du graph
+#------------------------
 
 # Traitement des nodes
 RULE_REGEX = re.compile("^R+[0-9]")
@@ -43,96 +53,54 @@ for node in NODE_LIST_ALL_INFO:
     else:
         SPECIES_LIST_ALL_INFO.append(node)   
 
-# Affichage des nodes 
-LABELS_TO_ID = {}
-NODE_ID_COORD = {}
+#---Récupération des labels-id
+#   Liste des éléments : LABELS_TO_ID = Name -> id
+LABELS_TO_ID = mcg.labels(RULES_LIST_ALL_INFO, SPECIES_LIST_ALL_INFO)
 
-#--Affichage des rules (rectangle)
-BOXES = {}
-for rule in RULES_LIST_ALL_INFO:
-    #z = randrange(0, 500)
-    #rules = box(pos = (rule[1][2][1][0][1], rule[1][2][1][1][1], z) , \
-    #            length=30, height=30, width=30, color=color.blue)
-    rules = box(pos = (rule[1][2][1][0][1], rule[1][2][1][1][1]) , \
-                length=30, height=30, width=30, color=color.blue)    
-    BOXES[rule[1][0][1]] = rules    
-    LABELS_TO_ID[rule[1][1][1]] = rule[1][0][1]
-    coord = {}
-    coord['x'] = rule[1][2][1][0][1]
-    coord['y'] = rule[1][2][1][1][1]
-    #coord['z'] = z    
-    NODE_ID_COORD[rule[1][0][1]] = coord
+#---Création des règles
+#   Liste des rules : BOXES = id -> objet.visual.box
+BOXES = mcg.affichage_rules(RULES_LIST_ALL_INFO)
 
-#--Affichage des species (sphere)
-SPHERES = {}    
-for specie in SPECIES_LIST_ALL_INFO:
-    #z = randrange(0, 500)    
-    #species = sphere(pos = (specie[1][2][1][0][1], specie[1][2][1][1][1], z), \
-    #        radius=20, color=color.green) 
-    species = sphere(pos = (specie[1][2][1][0][1], specie[1][2][1][1][1]), \
-            radius=20, color=color.green)    
-    SPHERES[specie[1][0][1]] = species
-    LABELS_TO_ID[specie[1][1][1]] = specie[1][0][1]
-    coord = {}
-    coord['x'] = specie[1][2][1][0][1]
-    coord['y'] = specie[1][2][1][1][1]
-    #coord['z'] = z    
-    NODE_ID_COORD[specie[1][0][1]] = coord
+
+#---Création des especes
+#   Liste des species : SPHERES = id -> objet.visual.sphere
+SPHERES = mcg.affichage_species(SPECIES_LIST_ALL_INFO)
+
+
+# Traitement des edges
+#---Récupération des positions de début et fin
+NODE_ID_COORD = mcg.coordonnes(RULES_LIST_ALL_INFO, SPECIES_LIST_ALL_INFO)
+
+#---Création des flèches
+#   Liste des liens : POINTERS_ID = numéro -> source_id, target_id
+#   Liste des arrows : POINTERS = numéro -> objet.visual.arrows
+POINTERS = mcg.affichage_arrows(EDGE_LIST_ALL_INFO, NODE_ID_COORD)[1]
+POINTERS_ID = mcg.affichage_arrows(EDGE_LIST_ALL_INFO, NODE_ID_COORD)[0]
 #------------------------------------------------------------------------------#
 
 #------------------------------------------------------------------------------#
-# EDGES
-#----------------------
-   
-# Traitement et affichage des edges
-POINTERS = {}
-POINTERS_ID = {}
-CPT = 0
-for edge in EDGE_LIST_ALL_INFO:
-    CPT = CPT + 1    
-    coord = {}    
-    source = edge[1][0][1] 
-    target = edge[1][1][1]
-    coord['source'] = source
-    coord['target'] = target
-    #pointer = arrow(\
-   #          pos = (NODE_ID_COORD[source]['x'], \
-   #                 NODE_ID_COORD[source]['y'], \
-   #                 NODE_ID_COORD[source]['z']), \
-   #          axis = (NODE_ID_COORD[target]['x'] - NODE_ID_COORD[source]['x'], \
-   #                  NODE_ID_COORD[target]['y'] - NODE_ID_COORD[source]['y'], \
-   #                  NODE_ID_COORD[target]['z'] - NODE_ID_COORD[source]['z']),\
-   #          shaftwidth = 5, color = (0.5, 0.5, 0.5))
-    pointer = arrow(\
-              pos = (NODE_ID_COORD[source]['x'], \
-                     NODE_ID_COORD[source]['y']), \
-             axis = (NODE_ID_COORD[target]['x'] - NODE_ID_COORD[source]['x'], \
-                     NODE_ID_COORD[target]['y'] - NODE_ID_COORD[source]['y']), \
-              shaftwidth = 5, color = (0.5, 0.5, 0.5))
-    POINTERS_ID[CPT] = coord
-    POINTERS[CPT] = pointer 
+# Lecture des simulation
+#------------------------
 
-#------------------------------------------------------------------------------#
+# Lecture des POE
+#---Import des données
+POE = csv.reader(open\
+ ("./Visu/Simulations/res_mutation_nwin_100_winsize_500000_nsim_500_Ri_96.poe",\
+ "rb"), delimiter='\t', quotechar='.')
+SPECIES_LIST_NAMES = csv.reader(open\
+    ("./Visu/Modele/model_14_03_2010_listOfSpecies.csv", "rb"))
 
-#------------------------------------------------------------------------------#
-# SUM-UP DATA
-#----------------------
+#---Traitement des données
+POE_VALUE = mrs.poe_treatment(POE, SPECIES_LIST_NAMES)
 
-# Liste des éléments : LABELS_TO_ID = Name -> id
-# Liste des rules : BOXES = id -> objet.visual.box
-# Liste des species : SPHERES = id -> objet.visual.sphere
-# Liste des liens : POINTERS_ID = numéro -> source_id, target_id
-# Liste des arrows : POINTERS = numéro -> objet.visual.arrows
+# Lecture des POR
+#---Import des données
+POR = csv.reader(open\
+ ("./Visu/Simulations/res_mutation_nwin_100_winsize_500000_nsim_500_Ri_96.por",\
+ "rb"), delimiter='\t', quotechar='.')
+RULES_LIST_NAMES = csv.reader(open\
+    ("./Visu/Modele/model_14_03_2010_listOfRules_modified.csv", "rb"))
 
-
-
-
-
-
-
-
-
-
-
-
+#---Traitement des données
+POR_VALUE = mrs.por_treatment(POR, RULES_LIST_NAMES)
 
