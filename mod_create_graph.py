8e931aa7dc40cd846efe88c8c0928a12abaa3537
Mathilde Daures <mathilde.daures@gmail.com>
2011-12-19 23:17:06 +0100

    fin 19/12

--- mod_create_graph.py
+++ mod_create_graph.py
@@ -5,122 +5,122 @@
     dico.
 """
 
-import pickle
-import re
 from visual import box, color, sphere, arrow
 #from random import randrange
 
 
-def recuperation_labels(RULES_LIST_ALL_INFO, SPECIES_LIST_ALL_INFO):
+def labels(rules, species):
     """ Récupère les labels
         Liste des éléments : LABELS_TO_ID = Name -> id
     """
-    LABELS_TO_ID = {}
+    labels_to_id = {}
     
-    for rule in RULES_LIST_ALL_INFO:
-        LABELS_TO_ID[rule[1][1][1]] = rule[1][0][1]
-    for specie in SPECIES_LIST_ALL_INFO:
-        LABELS_TO_ID[specie[1][1][1]] = specie[1][0][1]
+    for rule in rules:
+        labels_to_id[rule[1][1][1]] = rule[1][0][1]
+    for specie in species:
+        labels_to_id[specie[1][1][1]] = specie[1][0][1]
 
-    return LABELS_TO_ID
+    return labels_to_id
 
 
-def recupération_coordonnées(RULES_LIST_ALL_INFO, SPECIES_LIST_ALL_INFO):
+def coordonnes(rules, species):
     """ Récupération des coordonnées pour faire les flèches
     """
-    NODE_ID_COORD = {}
+    node_id_coord = {}
     
-    for rule in RULES_LIST_ALL_INFO:
+    for rule in rules:
         coord = {}
         coord['x'] = rule[1][2][1][0][1]
         coord['y'] = rule[1][2][1][1][1]
         #coord['z'] = z    
-        NODE_ID_COORD[rule[1][0][1]] = coord
-    for specie in SPECIES_LIST_ALL_INFO:
+        node_id_coord[rule[1][0][1]] = coord
+    for specie in species:
         coord = {}
         coord['x'] = specie[1][2][1][0][1]
         coord['y'] = specie[1][2][1][1][1]
         #coord['z'] = z    
-        NODE_ID_COORD[specie[1][0][1]] = coord
+        node_id_coord[specie[1][0][1]] = coord
 
-    return NODE_ID_COORD
+    return node_id_coord
 
 #------------------------------------------------------------------------------#
 # NODES
 #----------------------
 
-def affichage_rules(RULES_LIST_ALL_INFO):
+def affichage_rules(rules):
     """ Création des éléments pour les régles
         Liste des rules : BOXES = id -> objet.visual.box
     """    
-    BOXES = {}
+    boxes = {}
     
-    for rule in RULES_LIST_ALL_INFO:
+    for rule in rules:
         #z = randrange(0, 500)
-        #rules = box(pos = (rule[1][2][1][0][1], rule[1][2][1][1][1], z) , \
+        #regles = box(pos = (rule[1][2][1][0][1], rule[1][2][1][1][1], z) , \
         #            length=30, height=30, width=30, color=color.blue)
-        rules = box(pos = (rule[1][2][1][0][1], rule[1][2][1][1][1]) , \
+        regles = box(pos = (rule[1][2][1][0][1], rule[1][2][1][1][1]) , \
                     length=30, height=30, width=30, color=color.blue)    
-        BOXES[rule[1][0][1]] = rules
+        boxes[rule[1][0][1]] = regles
 
-    return BOXES    
+    return boxes    
 
 
-def affichage_species(SPECIES_LIST_ALL_INFO):
+def affichage_species(species):
     """ Création des éléments pour les espèces
         Liste des species : SPHERES = id -> objet.visual.sphere
     """
-    SPHERES = {}    
+    spheres = {}    
 
-    for specie in SPECIES_LIST_ALL_INFO:
-        #z = randrange(0, 500)    
-        #species = sphere(pos = (specie[1][2][1][0][1], specie[1][2][1][1][1], z), \
-        #        radius=20, color=color.green) 
-        species = sphere(pos = (specie[1][2][1][0][1], specie[1][2][1][1][1]), \
+    for specie in species:
+       #z = randrange(0, 500)    
+   #especes = sphere(pos = (specie[1][2][1][0][1], \specie[1][2][1][1][1], z), \
+   #        radius=20, color=color.green) 
+        especes = sphere(pos = (specie[1][2][1][0][1], specie[1][2][1][1][1]), \
                 radius=20, color=color.green)    
-        SPHERES[specie[1][0][1]] = species
+        spheres[specie[1][0][1]] = especes
 
-    return SPHERES
+    return spheres
         
 #------------------------------------------------------------------------------#
 # EDGES
 #----------------------
 
-def affichage_arrows(EDGE_LIST_ALL_INFO):
+def affichage_arrows(edges, node_coord):
     """ Création des éléments pour les liens entre règles et espèces
         Liste des liens : POINTERS_ID = numéro -> source_id, target_id
         Liste des arrows : POINTERS = numéro -> objet.visual.arrows
     """   
 
-    POINTERS = {}
-    POINTERS_ID = {}    
-    CPT = 0
+    pointers = {}
+    pointers_id = {}    
+    cpt = 0
 
-    for edge in EDGE_LIST_ALL_INFO:
-        CPT = CPT + 1    
+    for edge in edges:
+        cpt = cpt + 1    
         coord = {}    
         source = edge[1][0][1] 
         target = edge[1][1][1]
         coord['source'] = source
         coord['target'] = target
         #pointer = arrow(\
-       #          pos = (NODE_ID_COORD[source]['x'], \
-       #                 NODE_ID_COORD[source]['y'], \
-       #                 NODE_ID_COORD[source]['z']), \
-       #          axis = (NODE_ID_COORD[target]['x'] - NODE_ID_COORD[source]['x'], \
-       #                  NODE_ID_COORD[target]['y'] - NODE_ID_COORD[source]['y'], \
-       #                  NODE_ID_COORD[target]['z'] - NODE_ID_COORD[source]['z']),\
-       #          shaftwidth = 5, color = (0.5, 0.5, 0.5))
+   #          pos = (node_coordD[source]['x'], \
+   #                 node_coord[source]['y'], \
+   #                 node_coord[source]['z']), \
+   #          axis = (node_coord[target]['x'] - node_coord[source]['x'], \
+   #                  node_coord[target]['y'] - node_coord[source]['y'], \
+   #                  node_coord[target]['z'] - node_coord[source]['z']),\
+   #          shaftwidth = 5, color = (0.5, 0.5, 0.5))
         pointer = arrow(\
-                  pos = (NODE_ID_COORD[source]['x'], \
-                         NODE_ID_COORD[source]['y']), \
-                 axis = (NODE_ID_COORD[target]['x'] - NODE_ID_COORD[source]['x'], \
-                         NODE_ID_COORD[target]['y'] - NODE_ID_COORD[source]['y']), \
+                  pos = (node_coord[source]['x'], \
+                         node_coord[source]['y']), \
+                 axis = (node_coord[target]['x'] - \
+                            node_coord[source]['x'], \
+                         node_coord[target]['y'] - \
+                            node_coord[source]['y']), \
                   shaftwidth = 5, color = (0.5, 0.5, 0.5))
-        POINTERS_ID[CPT] = coord
-        POINTERS[CPT] = pointer 
+        pointers_id[cpt] = coord
+        pointers[cpt] = pointer 
 
-    return POINTERS_ID, POINTERS
+    return pointers_id, pointers
 
 
 
