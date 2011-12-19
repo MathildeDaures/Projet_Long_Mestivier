52085ae655011432db004ff9d491f3138847206a
Mathilde Daures <mathilde.daures@gmail.com>
2011-12-19 23:17:15 +0100

    fin 19/12

---
+++ mod_modif_graph.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+"""
+
+def read_color(colors_txt):
+    """
+    """
+    filin = open(colors_txt, 'r')
+    lignes = filin.readlines()
+    filin.close()
+   
+    cpt = 1
+    case = {}
+    
+    for li in lignes:
+        content = {}
+        for 
+    
